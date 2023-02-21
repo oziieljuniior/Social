f8888d0fcfaa07d5e5c401c088bb5b164bc55010
Oziel Junior <58310583+oziieljuniior@users.noreply.github.com>
2023-02-21 19:45:39 -0400

    Att

--- Django_Login/django_auth/django_auth/settings.py
+++ Django_Login/django_auth/django_auth/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@zykg2mdnq@#0(2(a#+u45u(4m&)5_kqe4$g86-fioy)1pihkb"
+SECRET_KEY = ##secretkeyhere
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
